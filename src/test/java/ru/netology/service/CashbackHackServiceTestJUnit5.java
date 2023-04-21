2060ae3202797b2e59c60514f5373bac4a9f1089
Gennadii Lezhnev <genlezhnev@gmail.com>
2023-04-22 00:02:30 +0300

    JUnit4-platform change packages

--- src/test/java/ru/netology/service/CashbackHackServiceTestJUnit5.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTestJUnit5.java
@@ -1,8 +1,7 @@
 package ru.netology.service;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CashbackHackServiceTestJUnit5 {
