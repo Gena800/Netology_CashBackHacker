35e35f04f40d20a36f19ab0c7c571f6d34826afa
Gennadii Lezhnev <genlezhnev@gmail.com>
2023-04-21 23:18:07 +0300

    JUnit4-platform add tests on JUnit5

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTestJUnit5.java
@@ -0,0 +1,64 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CashbackHackServiceTestJUnit5 {
+
+    @Test
+    public void ifAmountLessBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+        int expected = 500;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void ifAmount0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void ifAmountEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void ifAmountMoreBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+        int expected = 500;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void boundaryValuesLessBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void boundaryValuesMoreBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int expected = 999;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+
+    }
+}
